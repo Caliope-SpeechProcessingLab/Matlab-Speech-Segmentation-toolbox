617be57b85a31500ed53916d54b3d8ab1456f836
Salvador Florido Llorens <sal@uma.es>
2018-06-08 14:15:26 +0200

    Modificación e inserción de codigo en el evaluador ecDist, en el main, varias funciones auxiliares añadidas, y he renombrado el archicvo segmentador de fases porque tenia una errata su nombre de archivo

--- Source/main.m
+++ Source/main.m
@@ -1,8 +1,10 @@
 %--------------------------------------------MAIN-DEPICTION------------------------------------------------------------%
 %----------------------------------------------------------------------------------------------------------------------%
 %----------------------------------------------------------------------------------------------------------------------%
+clear all
 addpath('segment');
 addpath('aux_Func');
+addpath('eval');
 
 %% CONFIGURABLE SETTING VARIABLES:-----------------------------------------------------------------------------------------------------------%
 
@@ -14,7 +16,7 @@ plotType=input('Plot procedure: ','s'); % specifies type of plot.
 
 %Folder and audio file selection
 
-path='data/todasJasa_Q/';
+path='../data/todasJasa_Q/';
 
 % You can use regular expresions so select specific files b*.wav 
 fileSelection='b*.wav';
@@ -23,6 +25,7 @@ fileSelection='b*.wav';
 %and extension.
 list = dir([path fileSelection]);
 nfiles=length(list);
+
 
 %% ------------------------------------Storing in a cell array all wav-files data:-------------------------------------------------------------------------------------%
 speech_audios=cell(3,nfiles);
@@ -36,32 +39,41 @@ for ifile=1:nfiles
     speech_audios{3,ifile}=Fs;
     
 end
+
+%% ------------------------------------Adjunting Fs to Fs_standard=16000 Hz for all speech files------------------------------------------------------------------------------------------------%
+for ifile=1:nfiles
+    if speech_audios{3,ifile}>16000
+        newFs=16000;
+        [xD]=LP_decimation_function(speech_audios{1,ifile},speech_audios{3,ifile},newFs);
+        speech_audios{3,ifile}=newFs;
+        x=xD;
+    end
+end
 %% ------------------------------------------------------SEGMENTATION--------------------------------------------------------------------------------------------------%
 
 %Cell array where I can store all boundaries values from all segments per wav-files:
-boundaries_per_Audio=cell(3,nfiles);
+boundaries_per_audio=cell(3,nfiles);
 
-if strcmp(sT,'fixed')
+if strcmp(segType,'fixed')
     
     for ifile=1:nfiles
         [boundaries,Fs_S]=fixed_segmenter(speech_audios{1,ifile},speech_audios{3,ifile});
-        boundaries_per_Audio{1,ifile}=boundaries;  %Boundaries are explained on the depiction section.
-        boundaries_per_Audio{2,ifile}=speech_audios{2,ifile}; %File names
-        boundaries_per_Audio{3,ifile}=Fs_S;  %Output Frequency sampling after segmentation process
-        
+        boundaries_per_audio{1,ifile}=boundaries;  %Boundaries are explained on the depiction section.
+        boundaries_per_audio{2,ifile}=speech_audios{2,ifile}; %File names
+        boundaries_per_audio{3,ifile}=Fs_S;  %Output Frequency sampling after segmentation process 
     end
     
 end
 
-if strcmp(sT,'phase')
+if strcmp(segType,'phased')
     
     for ifile=1:nfiles
         
         [boundaries,Fs_S,NF]=phased_segmenter(speech_audios{1,ifile},speech_audios{3,ifile});
         
-        boundaries_per_Audio{1,ifile}=boundaries; %Boundaries are explained on the depiction section.
-        boundaries_per_Audio{2,ifile}=speech_audios{2,ifile}; %File names
-        boundaries_per_Audio{3,ifile}=Fs_S;  %Output Frequency sampling after segmentation process
+        boundaries_per_audio{1,ifile}=boundaries; %Boundaries are explained on the depiction section.
+        boundaries_per_audio{2,ifile}=speech_audios{2,ifile}; %File names
+        boundaries_per_audio{3,ifile}=Fs_S;  %Output Frequency sampling (of boundaries) after segmentation process
         
     end
     
@@ -73,14 +85,14 @@ end
 %% -------------------------------------------------------EVALUATION----------------------------------------------------------------------------------------------------%
 
 %Cell array where I can store all boundaries values from all segments per wav-files:
-ecDist_per_Audio=cell(2,nfiles);
+ecDist_per_audio=cell(2,nfiles);
 
-if strcmp(sE,'euclidean distance')
+if strcmpi(evalType,'e')
     
     for ifile=1:nfiles
-        ecDist=ecDist_evaluator(boundaries_per_Audio{1,ifile},boundaries_per_Audio{3,ifile});
-        ecDist_per_Audio{1,ifile}=ecDist; 
-        ecDist_per_Audio{2,ifile}=boundaries_per_Audio{2,ifile}; %File names
+        ecDist=ecDist_evaluator(speech_audios{1,ifile},boundaries_per_audio{1,ifile},boundaries_per_audio{3,ifile});
+        ecDist_per_audio{1,ifile}=ecDist; %Euclidean distances per file.
+        ecDist_per_audio{2,ifile}=boundaries_per_audio{2,ifile}; %File names.
     end
     
 end
